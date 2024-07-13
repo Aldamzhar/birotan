e7a7aadd74975b1db9cc6591e6ec516f07e378b8
Aldamzhar <aldamzhar.kiikbayev@nu.edu.kz>
2024-07-13 15:25:11 +0500

    правки от 7.07.24 + бэкграунд с узорами

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,12 +2,13 @@
 @include('title')
 @include('header')
 
-<div class="background">
-    <div class="overlay">
-        <h1><span class="gold">Бір</span> ел,</h1>
-        <h1><span class="gold">Бір</span> ұлт,</h1>
-        <h1><span class="gold">Бір</span> мақсат!</h1>
-        <a href="{{ url('/about') }}"><button>Көбірек білу</button></a>
+<div class="background" style="position: relative; width: 100%; height: calc(100vh - 60px); overflow: hidden;">
+    <img src="{{ asset('storage/background.png') }}" alt="Background Image" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover; z-index: -1;">
+    <div class="overlay" style="position: relative; text-align: left; margin-left: -25%;">
+        <h1 style="font-size: 6em; color: #0D2945; margin: 0.1em 0;"><span class="gold" style="color: #c8985d;">Бір</span> ел,</h1>
+        <h1 style="font-size: 6em; color: #0D2945; margin: 0.1em 0;"><span class="gold" style="color: #c8985d;">Бір</span> ұлт,</h1>
+        <h1 style="font-size: 6em; color: #0D2945; margin: 0.1em 0;"><span class="gold" style="color: #c8985d;">Бір</span> мақсат!</h1>
+        <a href="{{ url('/about') }}"><button style="margin-left: 120%; padding: 1em 2.5em; font-size: 1em; font-weight: bold; color: white; background-color: #0D2945; border: none; border-radius: 25px; cursor: pointer; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); font-family: 'Montserrat Alternates', serif; text-transform: uppercase;">Көбірек білу</button></a>
     </div>
 </div>
 </body>
@@ -31,7 +32,7 @@
         position: relative;
         width: 100%;
         height: calc(100vh - 60px); /* Adjust according to header height */
-        background-image: url('/storage/app/public/img_12.png'); /* Change to your ornament image path */
+        background-image: url('/storage/app/public/background.png'); /* Change to your ornament image path */
         background-repeat: repeat;
         background-size: contain;
         display: flex;
