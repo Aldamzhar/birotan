a48b8d0b1322ad23f6affdfaf973f645108319cd
Aldamzhar <aldamzhar.kiikbayev@nu.edu.kz>
2025-02-16 11:08:40 +0500

    оферта и tiptop информация

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,18 +1,30 @@
+@php use Illuminate\Support\Facades\Storage; @endphp
 <footer class="footer">
     <div class="footer-section-first">
-        <p>Birotan.kz сайты – қазақтілді ақпараттық, ғылыми-танымдық мазмұндағы ресурс. <br>Сайтта «БірОтан лексика» ғылыми-ағартушылық жобасы ұсынылған және әлемдегі, <br>еліміздегі басты технологиялық жаңалықтар жарияланады.</p>
+        <p>Birotan.kz сайты – қазақтілді ақпараттық, ғылыми-танымдық мазмұндағы ресурс. <br>Сайтта «БірОтан лексика»
+            ғылыми-ағартушылық жобасы ұсынылған және әлемдегі, <br>еліміздегі басты технологиялық жаңалықтар
+            жарияланады.</p>
         <p><b>© 2023 BirOtan.kz. Барлық құқықтар қорғалған</b></p>
     </div>
     <div class="footer-section-second">
         <p><i>Мекенжайы: 010000, Астана қаласы</i><br>
-                <i>Байланыс телефоны: +7 (701) ХХХ ХХХХ</i><br>
-                    <i>E-mail: info@birotan.kz</i>
+            <i>Байланыс телефоны: +7 (701) ХХХ ХХХХ</i><br>
+            <i>E-mail: info@birotan.kz</i>
+        </p>
+        <p>
+            <a href="{{Storage::disk('public')->url('TipTop_Информация_для_размещения_на_сайте_5_02_2025.docx')}}"
+               target="_blank">
+                TipTop Информация для размещения на сайте
+            </a><br>
+            <a href="{{Storage::disk('public')->url('проект_Договора_оферты_и_Политика_конфиденциальности_5_02_2025.docx')}}"
+               target="_blank">
+                Проект Договора оферты и Политика конфиденциальности
+            </a>
         </p>
     </div>
 </footer>
 
 <style>
-
     .footer-img {
        width: 20%;
        height: 20%;
@@ -26,18 +38,12 @@
         display: flex;
         justify-content: space-between;
         align-items: center;
-        /*padding: 10px;*/
-        /*margin: 20px 40px;*/
-        /*padding-left: 20px;*/
-        /*padding-top: 30px;*/
         background-color: white;
         color: #000;
         font-family: Inter, serif;
         font-size: 15px;
-        /*position: absolute;*/
         left: 0;
         bottom: 0;
-        /*height: 100px; !* Set the footer height *!*/
     }
 
     .footer-section-first {
@@ -48,5 +54,14 @@
     .footer-section-second {
         white-space: nowrap;
         margin-right: 180px;
+    }
+
+    .footer-section-second a {
+        color: #007bff;
+        text-decoration: none;
+    }
+
+    .footer-section-second a:hover {
+        text-decoration: underline;
     }
 </style>
